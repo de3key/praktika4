c855987ef432cbb015ec97d25d459c4aceee9515
de3key <gnosthard@gmail.com>
2021-05-19 10:30:36 +0700

    практика4

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число с 1 до 15 перевду в двоичную систему");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            switch (a)
+                    {
+                case  1:
+                    {
+                        Console.WriteLine("1");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("10");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("11");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("100");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("101");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("110");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("111");
+                        break;
+                    }
+                case 8:
+                    {
+                        Console.WriteLine("1000");
+                        break;
+                    }
+                case 9:
+                    {
+                        Console.WriteLine("1001");
+                        break;
+                    }
+                case 10:
+                    {
+                        Console.WriteLine("1010");
+                        break;
+                    }
+                case 11:
+                    {
+                        Console.WriteLine("1011");
+                        break;
+                    }
+                case 12:
+                    {
+                        Console.WriteLine("1100");
+                        break;
+                    }
+                case 13:
+                    {
+                        Console.WriteLine("1101");
+                        break;
+                    }
+                case 14:
+                    {
+                        Console.WriteLine("1110");
+                        break;
+                    }
+                case 15:
+                    {
+                        Console.WriteLine("1111");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ну короче ты что то не то ввел и получил шлак");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int p = 1;
+
+            for (int i = a; i <= b; i++)
+            {
+                p *= i;
+            }
+
+            Console.WriteLine("Ну что то получается: " + p);
+            Console.ReadKey();
+        }
+    }
+}
